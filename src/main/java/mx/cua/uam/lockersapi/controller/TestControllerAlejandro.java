604ac96b2a478d3b43887267d4a87e9a1a94f926
Alejandro Ramos <alejandro.ramos@cua.uam.mx>
2026-03-17 13:33:27 -0600

    Controller Alejandro

---
+++ src/main/java/mx/cua/uam/lockersapi/controller/TestControllerAlejandro.java
@@ -0,0 +1,15 @@
+package mx.cua.uam.lockersapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestControllerAlejandro {
+
+    @GetMapping("/ping")
+    public String holaMundo() {
+        return "Hola Mundo - Alejandro Ramos.";
+    }
+}
